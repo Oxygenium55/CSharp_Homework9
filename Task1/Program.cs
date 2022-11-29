ed50aa1ebd800fc4b6651a7747c8657861e11c5d
Oxygen <grandnorik@mail.ru>
2022-11-29 22:21:46 +0600

    Task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,22 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+int GetNumber(string message)
+{
+    Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int NaturalNumbers(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumbers(n, m + 1)}, ");
+    return m;
+}
+
+int n = GetNumber("Введите натуральное число больше нуля: ");
+int m = 1;
+Console.WriteLine(NaturalNumbers(n, m));
