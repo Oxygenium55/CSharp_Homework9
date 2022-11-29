1cf017e1fe0e5c6e85bfcb80a8bf61ca51662f03
Oxygen <grandnorik@mail.ru>
2022-11-29 22:55:54 +0600

    Task3 done

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,32 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
 
+int GetNumber(string message)
+{
+    Console.Write(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+int N = GetNumber("Введите неотрицательное число N: ");
+int M = GetNumber("Введите неотрицательное число M: ");
+Akkerman(M, N);
+Console.Write($"A({M}, {N}) = {Akkerman(M, N)}");
+
+
